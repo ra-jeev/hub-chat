5378f416ea73d74c69544d673cc56b02a24477fe
Rajeev R Sharma <i.rarsh@gmail.com>
2024-08-19 13:52:56 +0530

    feat: add @nuxtjs/mdc package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   future: { compatibilityVersion: 4 },
 
   // https://nuxt.com/modules
-  modules: ['@nuxthub/core', '@nuxt/eslint', '@nuxt/ui'],
+  modules: ['@nuxthub/core', '@nuxt/eslint', '@nuxt/ui', "@nuxtjs/mdc"],
 
   // https://hub.nuxt.com/docs/getting-started/installation#options
   hub: {},
