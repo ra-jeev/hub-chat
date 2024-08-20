642a6d9a90521cf4382726e096d1adec7088b5bd
Rajeev R Sharma <i.rarsh@gmail.com>
2024-08-20 23:11:19 +0530

    feat: create RangeInput for combined slider & input

---
+++ app/components/RangeInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <UFormGroup :label="label" :ui="{ container: 'mt-2' }">
+    <template #hint>
+      <UInput
+        v-model="model"
+        type="number"
+        :min="min"
+        :max="max"
+        :step="step"
+      />
+    </template>
+    <URange v-model="model" :min="min" :max="max" :step="step" size="sm" />
+  </UFormGroup>
+</template>
+
+<script setup lang="ts">
+const model = defineModel({ type: Number, required: true });
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  min: {
+    type: Number,
+  },
+  max: {
+    type: Number,
+  },
+  step: {
+    type: Number,
+  },
+});
+</script>
