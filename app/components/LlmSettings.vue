9e5e2d500160cd1ecc104599f12f7099a67d8368
Rajeev R Sharma <i.rarsh@gmail.com>
2024-08-21 21:44:46 +0530

    feat: add models ids

--- app/components/LlmSettings.vue
+++ app/components/LlmSettings.vue
@@ -13,7 +13,13 @@
     <UDivider />
     <div class="p-4 flex-1 space-y-6 overflow-y-auto">
       <UFormGroup label="Model">
-        <USelectMenu v-model="llmParams.model" size="md" :options="models" />
+        <USelectMenu
+          v-model="llmParams.model"
+          size="md"
+          :options="models"
+          value-attribute="id"
+          option-attribute="name"
+        />
       </UFormGroup>
 
       <RangeInput
@@ -121,38 +127,92 @@ const accordionItems = [
 ];
 
 const models = [
-  'deepseek-coder-6.7b-base-awq',
-  'deepseek-coder-6.7b-instruct-awq',
-  'deepseek-math-7b-instruct',
-  'discolm-german-7b-v1-awq',
-  'falcon-7b-instruct',
-  'gemma-7b-it',
-  'hermes-2-pro-mistral-7b',
-  'llama-2-13b-chat-awq',
-  'llama-2-7b-chat-fp16',
-  'llama-2-7b-chat-int8',
-  'llama-3-8b-instruct',
-  'llama-3-8b-instruct-awq',
-  'llama-3.1-8b-instruct',
-  'llama-3.1-8b-instruct-awq',
-  'llama-3.1-8b-instruct-fp8',
-  'llamaguard-7b-awq',
-  'meta-llama-3-8b-instruct',
-  'mistral-7b-instruct-v0.1',
-  'mistral-7b-instruct-v0.1-awq',
-  'mistral-7b-instruct-v0.2',
-  'neural-chat-7b-v3-1-awq',
-  'openchat-3.5-0106',
-  'openhermes-2.5-mistral-7b-awq',
-  'phi-2',
-  'qwen1.5-0.5b-chat',
-  'qwen1.5-1.8b-chat',
-  'qwen1.5-14b-chat-awq',
-  'qwen1.5-7b-chat-awq',
-  'sqlcoder-7b-2',
-  'starling-lm-7b-beta',
-  'tinyllama-1.1b-chat-v1.0',
-  'una-cybertron-7b-v2-bf16',
-  'zephyr-7b-beta-awq',
+  {
+    name: 'deepseek-coder-6.7b-base-awq',
+    id: '@hf/thebloke/deepseek-coder-6.7b-base-awq',
+  },
+  {
+    name: 'deepseek-coder-6.7b-instruct-awq',
+    id: '@hf/thebloke/deepseek-coder-6.7b-instruct-awq',
+  },
+  {
+    name: 'deepseek-math-7b-base',
+    id: '@cf/deepseek-ai/deepseek-math-7b-base',
+  },
+  {
+    name: 'deepseek-math-7b-instruct',
+    id: '@cf/deepseek-ai/deepseek-math-7b-instruct',
+  },
+  {
+    name: 'discolm-german-7b-v1-awq',
+    id: '@cf/thebloke/discolm-german-7b-v1-awq',
+  },
+  { name: 'falcon-7b-instruct', id: '@cf/tiiuae/falcon-7b-instruct' },
+  { name: 'gemma-2b-it-lora', id: '@cf/google/gemma-2b-it-lora' },
+  { name: 'gemma-7b-it-lora', id: '@cf/google/gemma-7b-it-lora' },
+  { name: 'gemma-7b-it', id: '@hf/google/gemma-7b-it' },
+  {
+    name: 'hermes-2-pro-mistral-7b',
+    id: '@hf/nousresearch/hermes-2-pro-mistral-7b',
+  },
+  { name: 'llama-2-13b-chat-awq', id: '@hf/thebloke/llama-2-13b-chat-awq' },
+  { name: 'llama-2-7b-chat-fp16', id: '@cf/meta/llama-2-7b-chat-fp16' },
+  {
+    name: 'llama-2-7b-chat-hf-lora',
+    id: '@cf/meta-llama/llama-2-7b-chat-hf-lora',
+  },
+  { name: 'llama-3-8b-instruct', id: '@cf/meta/llama-3-8b-instruct' },
+  { name: 'llama-3-8b-instruct-awq', id: '@cf/meta/llama-3-8b-instruct-awq' },
+  { name: 'llama-3.1-8b-instruct', id: '@cf/meta/llama-3.1-8b-instruct' },
+  {
+    name: 'llama-3.1-8b-instruct-awq',
+    id: '@cf/meta/llama-3.1-8b-instruct-awq',
+  },
+  {
+    name: 'llama-3.1-8b-instruct-fp8',
+    id: '@cf/meta/llama-3.1-8b-instruct-fp8',
+  },
+  { name: 'llamaguard-7b-awq', id: '@hf/thebloke/llamaguard-7b-awq' },
+  {
+    name: 'mistral-7b-instruct-v0.1',
+    id: '@cf/mistral/mistral-7b-instruct-v0.1',
+  },
+  {
+    name: 'mistral-7b-instruct-v0.1-awq',
+    id: '@hf/thebloke/mistral-7b-instruct-v0.1-awq',
+  },
+  {
+    name: 'mistral-7b-instruct-v0.2-lora',
+    id: '@cf/mistral/mistral-7b-instruct-v0.2-lora',
+  },
+  {
+    name: 'mistral-7b-instruct-v0.2',
+    id: '@hf/mistral/mistral-7b-instruct-v0.2',
+  },
+  {
+    name: 'neural-chat-7b-v3-1-awq',
+    id: '@hf/thebloke/neural-chat-7b-v3-1-awq',
+  },
+  { name: 'openchat-3.5-0106', id: '@cf/openchat/openchat-3.5-0106' },
+  {
+    name: 'openhermes-2.5-mistral-7b-awq',
+    id: '@hf/thebloke/openhermes-2.5-mistral-7b-awq',
+  },
+  { name: 'phi-2', id: '@cf/microsoft/phi-2' },
+  { name: 'qwen1.5-0.5b-chat', id: '@cf/qwen/qwen1.5-0.5b-chat' },
+  { name: 'qwen1.5-1.8b-chat', id: '@cf/qwen/qwen1.5-1.8b-chat' },
+  { name: 'qwen1.5-14b-chat-awq', id: '@cf/qwen/qwen1.5-14b-chat-awq' },
+  { name: 'qwen1.5-7b-chat-awq', id: '@cf/qwen/qwen1.5-7b-chat-awq' },
+  { name: 'sqlcoder-7b-2', id: '@cf/defog/sqlcoder-7b-2' },
+  { name: 'starling-lm-7b-beta', id: '@hf/nexusflow/starling-lm-7b-beta' },
+  {
+    name: 'tinyllama-1.1b-chat-v1.0',
+    id: '@cf/tinyllama/tinyllama-1.1b-chat-v1.0',
+  },
+  {
+    name: 'una-cybertron-7b-v2-bf16',
+    id: '@cf/fblgit/una-cybertron-7b-v2-bf16',
+  },
+  { name: 'zephyr-7b-beta-awq', id: '@hf/thebloke/zephyr-7b-beta-awq' },
 ];
 </script>
