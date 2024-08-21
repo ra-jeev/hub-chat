41ff94673e3eca6af93f6cf7694c860835f27656
Rajeev R Sharma <i.rarsh@gmail.com>
2024-08-21 15:42:01 +0530

    feat: updated nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,18 +5,10 @@ export default defineNuxtConfig({
   future: { compatibilityVersion: 4 },
 
   // https://nuxt.com/modules
-  modules: ['@nuxthub/core', '@nuxt/eslint', '@nuxt/ui', "@nuxtjs/mdc"],
+  modules: ['@nuxthub/core', '@nuxt/eslint', '@nuxt/ui', '@nuxtjs/mdc'],
 
   // https://hub.nuxt.com/docs/getting-started/installation#options
   hub: {},
-
-  // Env variables - https://nuxt.com/docs/getting-started/configuration#environment-variables-and-private-tokens
-  runtimeConfig: {
-    public: {
-      // Can be overridden by NUXT_PUBLIC_HELLO_TEXT environment variable
-      helloText: 'Hello from the Edge 👋',
-    },
-  },
 
   // https://eslint.nuxt.com
   eslint: {
@@ -26,6 +18,7 @@ export default defineNuxtConfig({
         semi: true,
         commaDangle: 'only-multiline',
         braceStyle: '1tbs',
+        arrowParens: true,
       },
     },
   },
