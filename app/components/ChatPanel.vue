fad7aa4e51af40233983d03d0c3275a2df0a61fc
Rajeev R Sharma <i.rarsh@gmail.com>
2024-08-27 21:51:39 +0530

    feat: use the new components

--- app/components/ChatPanel.vue
+++ app/components/ChatPanel.vue
@@ -35,27 +35,8 @@
         </div>
         <AssistantMessage v-else :content="message.content" />
       </div>
-      <div v-if="loading === 'message'" class="flex items-center space-x-4">
-        <div class="w-12 h-12 p-2 rounded-full bg-blue-500/20">
-          <UIcon
-            name="i-heroicons-sparkles-solid"
-            class="w-8 h-8 text-blue-400"
-          />
-        </div>
-        <div class="space-y-4">
-          <USkeleton class="h-4 w-[250px]" />
-          <USkeleton class="h-4 w-[200px]" />
-        </div>
-      </div>
-      <div
-        v-if="chatHistory.length === 0"
-        class="flex flex-col gap-y-6 items-center justify-center h-full"
-      >
-        <NoChatsImg class="w-64 h-48" />
-        <div class="text-center text-gray-500 dark:text-gray-400">
-          Ask me anything, type to get started
-        </div>
-      </div>
+      <ChatLoadingSkeleton v-if="loading === 'message'" />
+      <NoChats v-if="chatHistory.length === 0" class="h-full" />
     </div>
     <UDivider />
     <div class="flex items-start p-3.5 relative">
