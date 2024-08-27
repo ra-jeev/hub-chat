359fc45bac7489346f0eb398ce3826c1351e6af0
Rajeev R Sharma <i.rarsh@gmail.com>
2024-08-27 21:49:04 +0530

    feat: move chat loading skeleton to separate component

---
+++ app/components/ChatLoadingSkeleton.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center space-x-4">
+    <div class="w-12 h-12 p-2 rounded-full bg-blue-500/20">
+      <UIcon name="i-heroicons-sparkles-solid" class="w-8 h-8 text-blue-400" />
+    </div>
+    <div class="space-y-4">
+      <USkeleton class="h-4 w-[250px]" />
+      <USkeleton class="h-4 w-[200px]" />
+    </div>
+  </div>
+</template>
