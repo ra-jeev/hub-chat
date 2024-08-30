06ad50f21cff846121a63494336c810885b35e82
Rajeev R Sharma <i.rarsh@gmail.com>
2024-08-31 00:09:42 +0530

    feat: make LLMParams fields optional (except model)

--- types/index.ts
+++ types/index.ts
@@ -1,13 +1,13 @@
 export type LlmParams = {
   model: string;
-  temperature: number;
-  maxTokens: number;
+  temperature?: number;
+  maxTokens?: number;
   topP?: number;
   topK?: number;
   frequencyPenalty?: number;
   presencePenalty?: number;
-  systemPrompt: string;
-  stream: boolean;
+  systemPrompt?: string;
+  stream?: boolean;
 };
 
 export type ChatMessage = {
