6a0f96998dc99ebc5e510e0b54bdd129413ad15f
Rajeev R Sharma <i.rarsh@gmail.com>
2024-08-30 23:31:11 +0530

    fix: seo description typo

--- app/app.vue
+++ app/app.vue
@@ -13,8 +13,10 @@ useHead({
 useSeoMeta({
   title: 'Hub Chat',
   ogTitle: 'Hub Chat - Cloudflare AI LLM Playground',
-  description: 'Hub Chat is aCloudflare Workers AI LLM playground with NuxtHub and Nuxt UI.',
-  ogImage: 'https://assets.hub.nuxt.com/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJodHRwczovL2h1Yi1jaGF0Lm51eHQuZGV2IiwiaWF0IjoxNzI0NTcyMzI3fQ.HqgnQrHVs34qdjFSP6RYy_M94duSAgPEvGgHF5j0xvI.jpg?theme=light',
+  description:
+    'Hub Chat is a Cloudflare Workers AI LLM playground with NuxtHub and Nuxt UI.',
+  ogImage:
+    'https://assets.hub.nuxt.com/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJodHRwczovL2h1Yi1jaGF0Lm51eHQuZGV2IiwiaWF0IjoxNzI0NTcyMzI3fQ.HqgnQrHVs34qdjFSP6RYy_M94duSAgPEvGgHF5j0xvI.jpg?theme=light',
   twitterCard: 'summary_large_image',
-})
+});
 </script>
