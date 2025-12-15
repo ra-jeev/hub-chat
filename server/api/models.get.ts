8c531474d7cfb13fbe74b87450830ef94e32d312
Rajeev R Sharma <i.rarsh@gmail.com>
2025-12-15 21:53:54 +0530

    chore: update models api endpoint to return models using env.AI

--- server/api/models.get.ts
+++ server/api/models.get.ts
@@ -1,38 +1,32 @@
 interface ModelResult {
-  success: boolean;
-  result: {
+  id: string;
+  source: number;
+  name: string;
+  description: string;
+  task: {
     id: string;
     name: string;
     description: string;
-    task: {
-      id: string;
-      name: string;
-      description: string;
-    };
-    propertys: {
-      property_id: string;
-      value: string;
-    }[];
+  };
+  tags: string[];
+  properties: {
+    property_id: string;
+    value: string;
   }[];
 }
 
 export default defineCachedEventHandler(
-  async () => {
-    const accountId = process.env.CLOUDFLARE_ACCOUNT_ID;
-    const userToken = process.env.CLOUDFLARE_API_TOKEN;
-    try {
-      const response = await $fetch<ModelResult>(
-        `https://api.cloudflare.com/client/v4/accounts/${accountId}/ai/models/search?task=text generation`,
-        {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${userToken}`,
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+  async (event) => {
+    const { cloudflare } = event.context;
+    const ai = cloudflare.env.AI;
 
-      return response.result.map((model) => model.name);
+    try {
+      const result = (await ai.models({
+        task: 'text generation',
+        per_page: 100, // should get all of them, else use pagination
+      })) as ModelResult[];
+
+      return result.map((model) => model.name);
     } catch (error) {
       console.error('Error fetching models:', error);
     }
